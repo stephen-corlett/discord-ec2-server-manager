41981f2c3f34e8d8a540ac2346864f5bce99b655
Stephen Corlett <scorlett817@gmail.com>
2021-10-01 15:31:07 -0700

    Fix configurations

--- hello-world/src/app.ts
+++ hello-world/src/app.ts
@@ -1,9 +1,11 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const queries = JSON.stringify(event.queryStringParameters);
   return {
     statusCode: 200,
-    body: `Queries: ${queries}`
-  }
+    body: `Queries: ${queries}`,
+  };
 };
+
+export default { lambdaHandler };
